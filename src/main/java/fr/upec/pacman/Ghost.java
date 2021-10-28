fabd884d31c8d20f9899cdb2c77d37e1421d9b99
Frédéric TRAN <frederictran@hotmail.fr>
2021-10-28 02:05:11 +0200

    SmotherPacman

--- src/main/java/fr/upec/pacman/Ghost.java
+++ src/main/java/fr/upec/pacman/Ghost.java
@@ -4,33 +4,13 @@ import java.awt.*;
 import java.util.Random;
 
 public class Ghost extends Entity {
-    private int dx;
-    private int dy;
     private int counter; //cmp? qu'est-ce que c'est? Compagnie du chemin de fer Métropolitain de Paris?
     private boolean turn;
 
     public Ghost(int x, int y, Color color) {
         super(x, y, color);
         this.turn = true;
-        this.dx = 4;
-        this.dy = 4;
         this.counter = 0;
-    }
-
-    public int getDx() {
-        return dx;
-    }
-
-    public void setDx(int dx) {
-        this.dx = dx;
-    }
-
-    public int getDy() {
-        return dy;
-    }
-
-    public void setDy(int dy) {
-        this.dy = dy;
     }
 
     public int getCounter() {
@@ -56,20 +36,20 @@ public class Ghost extends Entity {
     //Voir branche AlternativeWall
     public void randomMove() {
         Random r = new Random();
-        this.dx = 0;
-        this.dy = 0;
+        super.setDx(0);
+        super.setDy(0);
         switch (r.nextInt(4)) { //0 = up, 1 = right, 2 = down, 3 = left
             case 0:
-                this.dy = -4;
+                super.setDy(-4);
                 break;
             case 1:
-                this.dx = 4;
+                super.setDx(4);
                 break;
             case 2:
-                this.dy = 4;
+                super.setDy(4);
                 break;
             case 3:
-                this.dx = -4;
+                super.setDx(-4);
                 break;
         }
     }

15b8384c642de0302d28915d1e6860f26496f280
Frédéric TRAN <frederictran@hotmail.fr>
2022-01-04 02:29:00 +0100

    Simplification du code

--- src/main/java/fr/upec/pacman/Pacman.java
+++ src/main/java/fr/upec/pacman/Pacman.java
@@ -77,25 +77,19 @@ public class Pacman extends Entity {
 
     public void eatInvisible() {
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
-        Future future = executor.submit(new Runnable() {
-            @Override
-            public void run() {
-                long start = System.currentTimeMillis();
-                while (!Thread.interrupted()) {
-                    invisible = true;
-                    setColor(Color.decode("#FDFD96"));
-                    invisibleTimer = System.currentTimeMillis() - start;
-                }
-                invisible = false;
-                invisibleTimer = 0;
-                setColor(Color.decode("#fdff00"));
+        Future<?> future = executor.submit(() -> {
+            long start = System.currentTimeMillis();
+            while (!Thread.interrupted()) {
+                invisible = true;
+                setColor(Color.decode("#FDFD96"));
+                invisibleTimer = System.currentTimeMillis() - start;
             }
+            invisible = false;
+            invisibleTimer = 0;
+            setColor(Color.decode("#fdff00"));
         });
-        executor.schedule(new Runnable() {
-            @Override
-            public void run() {
-                future.cancel(true);
-            }
+        executor.schedule(() -> {
+            future.cancel(true);
         }, 10, TimeUnit.SECONDS);
         executor.shutdown();
     }
@@ -110,34 +104,28 @@ public class Pacman extends Entity {
 
     public void eatSuperPow() {
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
-        Future future = executor.submit(new Runnable() {
-            @Override
-            public void run() {
-                game.getP().getTimerGhost().setDelay(60);
-                long start = System.currentTimeMillis();
-                for (Ghost g : game.getGhosts()) {
-                    g.setColor(Color.BLUE);
-                }
-                while (!Thread.interrupted()) {
-                    superPow = true;
-                    setColor(Color.ORANGE);
-                    superPowTimer = System.currentTimeMillis() - start;
-                }
-                game.getP().getTimerGhost().setDelay(20);
-                superPow = false;
-                superPowTimer = 0;
-                setColor(Color.decode("#fdff00"));
-                game.getGhosts()[0].setColor(Color.decode("#ea82e5"));
-                game.getGhosts()[1].setColor(Color.decode("#46bfee"));
-                game.getGhosts()[2].setColor(Color.decode("#db851c"));
-                game.getGhosts()[3].setColor(Color.decode("#d03e19"));
+        Future<?> future = executor.submit(() -> {
+            game.getP().getTimerGhost().setDelay(60);
+            long start = System.currentTimeMillis();
+            for (Ghost g : game.getGhosts()) {
+                g.setColor(Color.BLUE);
             }
+            while (!Thread.interrupted()) {
+                superPow = true;
+                setColor(Color.ORANGE);
+                superPowTimer = System.currentTimeMillis() - start;
+            }
+            game.getP().getTimerGhost().setDelay(20);
+            superPow = false;
+            superPowTimer = 0;
+            setColor(Color.decode("#fdff00"));
+            game.getGhosts()[0].setColor(Color.decode("#ea82e5"));
+            game.getGhosts()[1].setColor(Color.decode("#46bfee"));
+            game.getGhosts()[2].setColor(Color.decode("#db851c"));
+            game.getGhosts()[3].setColor(Color.decode("#d03e19"));
         });
-        executor.schedule(new Runnable() {
-            @Override
-            public void run() {
-                future.cancel(true);
-            }
+        executor.schedule(() -> {
+            future.cancel(true);
         }, 10, TimeUnit.SECONDS);
         executor.shutdown();
     }
@@ -152,8 +140,7 @@ public class Pacman extends Entity {
     }
 
     public void eatMix() {
-        if (game.getMap().isBlockingWall()) game.getMap().setBlockingWall(false);
-        else game.getMap().setBlockingWall(true);
+        game.getMap().setBlockingWall(!game.getMap().isBlockingWall());
     }
 
     public boolean isAlive() {
